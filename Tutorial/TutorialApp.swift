59cfdcbcc779e4cebf543fd471042eccdd0eb959
TraianC299 <traianciochina@gmail.com>
2022-08-09 10:19:05 +0300

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Traian on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
